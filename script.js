2bf4973e2db3ca0e01939cca93632c260f803c77
brkan23 <birukanabawey@gmail.com>
2025-07-08 13:20:33 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector("form");
+  
+    form.addEventListener("submit", (event) => {
+      event.preventDefault();
+  
+      const name = form.elements[0].value;
+      const comment = form.elements[2].value;
+      const commentDiv = document.createElement("div");
+      commentDiv.classList.add("comment");
+  
+      const commentHeader = document.createElement("p");
+      commentHeader.textContent = name;
+  
+      const commentDate = document.createElement("p");
+      const currentDate = new Date().toLocaleDateString();
+      commentDate.textContent = Published on ${currentDate};
+  
+      const commentText = document.createElement("h2");
+      commentText.textContent = comment;
+  
+      commentDiv.appendChild(commentHeader);
+      commentDiv.appendChild(commentDate);
+      commentDiv.appendChild(commentText);
+  
+      
+      document.body.appendChild(commentDiv);
+  
+      form.reset();
+    });
+  });
